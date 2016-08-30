0582030e52595e9fbedd6cabb983f0509a382c1a
Matěj Suchánek <matejsuchanek@users.noreply.github.com>
2016-08-30 12:35:49 +0200

    fix errors, refact., add notes

--- captiontoimage.py
+++ captiontoimage.py
@@ -11,6 +11,7 @@ repo = site.data_repository()
 
 QUERY = "SELECT DISTINCT ?item WHERE { ?item wdt:P2096 [] }"
 
+bad_cache = ['P2096']
 good_cache = []
 
 for item in pagegenerators.WikidataSPARQLPageGenerator(QUERY, site=site):
@@ -19,13 +20,17 @@ for item in pagegenerators.WikidataSPARQLPageGenerator(QUERY, site=site):
         continue
 
     our_prop = 'P18'
-    if our_prop not inn item.claims.keys():
+    if our_prop not in item.claims.keys():
         our_prop = None
         for prop in item.claims.keys():
+            if prop in bad_cache:
+                continue
             if prop not in good_cache:
                 prop_page = pywikibot.PropertyPage(repo, prop)
                 if prop_page.type == "commonsMedia":
                     good_cache.append(prop)
+                else:
+                    bad_cache.append(prop)
 
             if prop in good_cache:
                 if our_prop is not None:
@@ -36,6 +41,7 @@ for item in pagegenerators.WikidataSPARQLPageGenerator(QUERY, site=site):
 
     if our_prop is None:
         pywikibot.output("%s: No media property found" % item.title())
+        # todo: remove?
         continue
 
     if our_prop is False:
@@ -44,20 +50,23 @@ for item in pagegenerators.WikidataSPARQLPageGenerator(QUERY, site=site):
 
     remove_claims = []
     media_claim = item.claims[our_prop][0]
+    if len(item.claims[our_prop]) > 1:
+        pywikibot.output("%s: Property %s has more than one value" % (item.title(), our_prop))
+        continue
     for caption in item.claims['P2096']:
-        if 'P2096' not in media_claim.qualifiers.keys():
-            target = caption.getTarget()
+        if 'P2096' in media_claim.qualifiers.keys():
+            language = caption.getTarget().language
             has_same_lang = False
             for claim in media_claim.qualifiers['P2096']:
-                if claim.getTarget().language == target.language:
+                if claim.getTarget().language == language:
                     has_same_lang = True
                     break
             if has_same_lang is True:
-                pywikibot.output("%s: %s property already has a caption in language %s" % (item.title(), our_prop, target.language))
+                pywikibot.output("%s: Property %s already has a caption in language %s" % (item.title(), our_prop, language))
                 continue
 
         caption.isQualifier = True
-        media_claim.addQualifier(caption)
+        media_claim.addQualifier(caption) # summary/where from?
         remove_claims.append(caption)
 
     if len(remove_claims) > 0:
