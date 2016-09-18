46a3c797a7e647294055ebf5bc4ae8e2d845faa2
Matěj Suchánek <matejsuchanek@users.noreply.github.com>
2016-09-18 11:03:51 +0200

    use bot class in captiontoimage.py, introduce a new one

--- captiontoimage.py
+++ captiontoimage.py
@@ -1,77 +1,107 @@
 # -*- coding: utf-8  -*-
-import datetime
 import pywikibot
 
 from pywikibot import pagegenerators
+from pywikibot.bot import SkipPageError
 
-start = datetime.datetime.now()
+from scripts.wikidata import WikidataEntityBot
 
-site = pywikibot.Site('wikidata', 'wikidata')
-repo = site.data_repository()
+class CaptionToImageBot(WikidataEntityBot):
 
-QUERY = "SELECT DISTINCT ?item WHERE { ?item wdt:P2096 [] }"
+    '''
+    Bot re-adding file captions as qualifiers to the files on Wikidata
 
-bad_cache = ['P2096']
-good_cache = []
+    Supported parameters:
+    * -removeall - if a caption cannot be reused, remove it as well
+    '''
 
-for item in pagegenerators.WikidataSPARQLPageGenerator(QUERY, site=site):
-    item.get()
-    if 'P2096' not in item.claims.keys():
-        continue
+    def __init__(self, site, **kwargs):
+        self.availableOptions.update({
+            'removeall': False
+        })
+        kwargs['bad_cache'] = ['P2096']
+        super(CaptionToImageBot, self).__init__(site, **kwargs)
 
-    our_prop = 'P18'
-    if our_prop not in item.claims.keys():
-        our_prop = None
-        for prop in item.claims.keys():
-            if prop in bad_cache:
-                continue
-            if prop not in good_cache:
-                prop_page = pywikibot.PropertyPage(repo, prop)
-                if prop_page.type == "commonsMedia":
-                    good_cache.append(prop)
-                else:
-                    bad_cache.append(prop)
+    def filterProperty(self, prop_page):
+        return prop_page.type == "commonsMedia"
 
-            if prop in good_cache:
-                if our_prop is not None:
-                    our_prop = False
-                    break
-                else:
-                    our_prop = prop
+    def init_page(self, item):
+        super(CaptionToImageBot, self).init_page(item)
+        if 'P2096' not in item.claims.keys():
+            raise SkipPageError(
+                item,
+                "Missing P2096 property"
+            )
 
-    if our_prop is None:
-        pywikibot.output("%s: No media property found" % item.title())
-        # todo: remove?
-        continue
+    def treat_page(self):
+        item = self.current_page
+        our_prop = 'P18'
+        if our_prop not in item.claims.keys():
+            our_prop = None
+            for prop in item.claims.keys():
+                if checkProperty(prop):
+                    if our_prop is None:
+                        our_prop = prop
+                    else:
+                        pywikibot.output("More than one media property used")
+                        return
 
-    if our_prop is False:
-        pywikibot.output("%s: More than one media property used" % item.title())
-        continue
+        remove_claims = []
+        remove_all = self.getOption('removeall') is True
+        if our_prop is None:
+            pywikibot.output("No media property found")
+            if remove_all:
+                remove_claims.extend(item.claims['P2096'])
+                self._save_page(item, self._save_entity, item.removeClaims,
+                                remove_claims, summary="removing redundant property")
+            return
 
-    remove_claims = []
-    media_claim = item.claims[our_prop][0]
-    if len(item.claims[our_prop]) > 1:
-        pywikibot.output("%s: Property %s has more than one value" % (item.title(), our_prop))
-        continue
-    for caption in item.claims['P2096']:
-        if 'P2096' in media_claim.qualifiers.keys():
-            language = caption.getTarget().language
-            has_same_lang = False
-            for claim in media_claim.qualifiers['P2096']:
-                if claim.getTarget().language == language:
-                    has_same_lang = True
-                    break
-            if has_same_lang is True:
-                pywikibot.output("%s: Property %s already has a caption in language %s" % (item.title(), our_prop, language))
-                continue
+        media_claim = item.claims[our_prop][0]
+        if len(item.claims[our_prop]) > 1:
+            pywikibot.output("Property %s has more than one value" % our_prop)
+            return
 
-        caption.isQualifier = True
-        media_claim.addQualifier(caption) # summary/where from?
-        remove_claims.append(caption)
+        for caption in item.claims['P2096']:
+            if 'P2096' in media_claim.qualifiers.keys():
+                language = caption.getTarget().language
+                has_same_lang = False
+                for claim in media_claim.qualifiers['P2096']:
+                    if claim.getTarget().language == language:
+                        has_same_lang = True
+                        break
+                if has_same_lang is True:
+                    pywikibot.output("Property %s already has a caption in language %s" % (our_prop, language))
+                    if remove_all:
+                        remove_claims.append(caption)
+                    continue
 
-    if len(remove_claims) > 0:
-        site.removeClaims(remove_claims)
+            caption.isQualifier = True
+            self._save_page(item, self._save_entity, media_claim.addQualifier,
+                            caption)
+            remove_claims.append(caption)
 
-end = datetime.datetime.now()
+        if len(remove_claims) > 0:
+            self._save_page(item, self._save_entity, item.removeClaims,
+                            remove_claims, summary="removing redundant property")
 
-pywikibot.output("Complete! Took %s seconds" % (end - start).total_seconds())
+def main(*args):
+    options = {}
+    for arg in pywikibot.handle_args(args):
+        if arg.startswith('-'):
+            arg, sep, value = arg.partition(':')
+            if value != '':
+                options[arg[1:]] = value if not value.isdigit() else int(value)
+            else:
+                options[arg[1:]] = True
+
+    QUERY = "SELECT DISTINCT ?item WHERE { ?item wdt:P2096 [] }"
+
+    site = pywikibot.Site('wikidata', 'wikidata')
+
+    options['generator'] = pagegenerators.WikidataSPARQLPageGenerator(QUERY, site=site)
+
+    bot = CaptionToImageBot(site, **options)
+    bot.run()
+
+if __name__ == "__main__":
+    main()
---
+++ wikidata.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8  -*-
+import pywikibot
+
+from pywikibot.bot import (
+    SingleSiteBot, ExistingPageBot, NoRedirectPageBot, WikidataBot
+)
+
+class WikidataEntityBot(SingleSiteBot, ExistingPageBot, NoRedirectPageBot):
+
+    '''
+    Bot interacting and editing Wikidata entities
+    Features:
+    * Works around [[phab:T86074]]
+    * Caches properties so that iterating claims is faster
+    * Hacks into WikidataBot and gets sources
+
+    Planned:
+    * Item cleanup like missing and wrong labels etc.
+    '''
+
+    def __init__(self, site, **kwargs):
+        pywikibot.output("Please help fix [[phab:T86074]] to make Wikidata scripts simpler")
+        self.bad_cache = kwargs.pop('bad_cache', [])
+        self.good_cache = kwargs.pop('good_cache', [])
+        super(WikidataEntityBot, self).__init__(site, **kwargs)
+        self.repo = site.data_repository()
+
+    def init_page(self, item):
+        item.get()
+
+    def checkProperty(self, prop):
+        if prop in self.good_cache:
+            return True
+        if prop in self.bad_cache:
+            return False
+
+        self.cacheProperty(prop)
+        return self.checkProperty(prop)
+
+    def cacheProperty(self, prop):
+        prop_page = pywikibot.PropertyPage(self.repo, prop)
+        if self.filterProperty(prop_page) is True:
+            self.good_cache.append(prop)
+        else:
+            self.bad_cache.append(prop)
+
+    def filterProperty(self, prop_page):
+        raise NotImplementedError(
+            "%s.filterProperty needs overriding in a subclass" % self.__class__
+        )
+
+    def _save_entity(self, func, *args, **kwargs):
+        if 'async' in kwargs:
+            kwargs.pop('async') # fixme: T86074
+        func(*args, **kwargs)
+
+    def getSource(self):
+        if not hasattr(self, 'source'):
+            wd_bot = WikidataBot()
+            self.source = wd_bot.getSource(self.site)
+        return self.source
