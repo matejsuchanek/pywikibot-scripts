8c1e75d7e8d4e0dd8044e3186782f0e580931b0d
Matěj Suchánek <matejsuchanek97@gmail.com>
2018-04-30 21:34:39 +0200

    fix query_store to work in various environments

--- query_store.py
+++ query_store.py
@@ -5,7 +5,10 @@ class QueryStore(object):
 
     '''Interface for loading SPARQL queries from text files'''
 
-    def __init__(self, path='scripts\\userscripts\\myscripts\\queries'):
+    def __init__(self, path=None):
+        if path is None:
+            dirname = os.path.dirname(os.path.realpath(__file__))
+            path = os.join.path(dirname, 'queries')
         self.path = path
 
     def get_query(self, name):
