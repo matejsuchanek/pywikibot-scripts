0a53533b1dd3b3354702cd9b609b94f654b0465e
Matěj Suchánek <matejsuchanek97@gmail.com>
2019-01-18 11:56:19 +0100

    new script

---
+++ shift_ranks.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import pywikibot
+
+from pywikibot import pagegenerators
+
+from .query_store import QueryStore
+from .wikidata import WikidataEntityBot
+
+
+class RanksShiftingBot(WikidataEntityBot):
+
+    end_prop = 'P582'
+    reason_prop = 'P2241'
+    use_from_page = False
+
+    def __init__(self, generator, **kwargs):
+        self.availableOptions.update({
+            'limit': 500,
+        })
+        super(RanksShiftingBot, self).__init__(**kwargs)
+        self.store = QueryStore()
+        self._generator = generator or self.custom_generator()
+
+    def custom_generator(self):
+        query = self.store.build_query(
+            'shift_ranks',
+            limit=self.getOption('limit'),
+            prop=self.end_prop
+        )
+        return pagegenerators.WikidataSPARQLPageGenerator(query, site=self.repo)
+
+    @property
+    def generator(self):
+        return pagegenerators.PreloadingEntityGenerator(self._generator)
+
+    @property
+    def summary(self):
+        return ('undeprecate claims and shift other ranks, see '
+                '[[Special:MyLanguage/Help:Ranking|Help:Ranking]]')
+
+    def treat_page_and_item(self, page, item):
+        changed = []
+        for claims in item.claims.values():
+            by_rank = {
+                'preferred': [],
+                'normal': [],
+                'deprecated': [],
+            }
+            ok = False
+            for claim in claims:
+                by_rank[claim.rank].append(claim)
+                if claim.rank == 'preferred':
+                    if claim.qualifiers.get(self.end_prop):
+                        ok = False
+                        break
+                elif claim.rank == 'deprecated':
+                    if claim.qualifiers.get(self.reason_prop):
+                        ok = False
+                        break
+                    if not ok:
+                        ok = bool(claim.qualifiers.get(self.end_prop))
+            if not ok:
+                continue
+            for claim in by_rank['deprecated']:
+                if claim.qualifiers.get(self.end_prop):
+                    claim.setRank('normal')
+                    changed.append(claim)
+            if not by_rank['preferred']:
+                for claim in by_rank['normal']:
+                    if not claim.qualifiers.get(self.end_prop):
+                        claim.setRank('preferred')
+                        changed.append(claim)
+        if changed:
+            data = {'claims': [cl.toJSON() for cl in changed]}
+            self.user_edit_entity(item, data, summary=self.summary)
+
+
+def main(*args):
+    options = {}
+    local_args = pywikibot.handle_args(args)
+    site = pywikibot.Site()
+    genFactory = pagegenerators.GeneratorFactory(site=site)
+    for arg in local_args:
+        if genFactory.handleArg(arg):
+            continue
+        if arg.startswith('-'):
+            arg, sep, value = arg.partition(':')
+            if value != '':
+                options[arg[1:]] = int(value) if value.isdigit() else value
+            else:
+                options[arg[1:]] = True
+
+    generator = genFactory.getCombinedGenerator()
+    bot = RanksShiftingBot(generator=generator, site=site, **options)
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
