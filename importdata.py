b4bc4ea143b7059ca02de2d3fb08202057dcc658
Matěj Suchánek <matejsuchanek97@gmail.com>
2019-05-04 11:23:03 +0200

    update importdata.py

--- importdata.py
+++ importdata.py
@@ -12,13 +12,15 @@ site = pywikibot.Site('wikidata', 'wikidata')
 repo = site.data_repository()
 
 direct = pywikibot.input('File directory: ')
-date = pywikibot.WbTime(year=2018, month=1, day=1, site=repo)
+date = pywikibot.WbTime(year=2019, month=1, day=1, site=repo)
 
-ref_item = 'Q52417047'
+ref_item = 'Q63450605'
 
 with open(direct, 'r', encoding='utf-8') as file_data:
-    next(file_data)
+    next(file_data)  # header
     for line in file_data:
+        if not line:
+            continue
         split = line.split('\t')
         item = pywikibot.ItemPage(repo, split[0])
         item.get()
@@ -29,7 +31,8 @@ with open(direct, 'r', encoding='utf-8') as file_data:
             if claim.getRank() == 'preferred':
                 claim.setRank('normal')
                 upToDateClaims.append(claim)
-            if claim.qualifiers.get('P585') and claim.qualifiers['P585'][0].target_equals(date):
+            if (claim.qualifiers.get('P585')
+                    and claim.qualifiers['P585'][0].target_equals(date)):
                 hasNewClaim = True
                 break
 
@@ -40,35 +43,32 @@ with open(direct, 'r', encoding='utf-8') as file_data:
         newClaim.setTarget(pywikibot.WbQuantity(count, site=repo))
         newClaim.setRank('preferred')
 
-        data = {'claims':[newClaim.toJSON()]}
-        data['claims'][0]['qualifiers'] = {}
-        data['claims'][0]['references'] = [{'snaks':{}}]
-
-        newClaim_date = pywikibot.Claim(repo, 'P585', isQualifier=True)
+        newClaim_date = pywikibot.Claim(repo, 'P585', is_qualifier=True)
         newClaim_date.setTarget(date)
-        data['claims'][0]['qualifiers']['P585'] = [newClaim_date.toJSON()]
+        newClaim.addQualifier(newClaim_date)
 
-        newClaim_criter = pywikibot.Claim(repo, 'P1013', isQualifier=True)
+        newClaim_criter = pywikibot.Claim(repo, 'P1013', is_qualifier=True)
         newClaim_criter.setTarget(pywikibot.ItemPage(repo, 'Q2641256'))
-        data['claims'][0]['qualifiers']['P1013'] = [newClaim_criter.toJSON()]
+        newClaim.addQualifier(newClaim_criter)
 
-        newClaim_men = pywikibot.Claim(repo, 'P1540', isQualifier=True)
+        newClaim_men = pywikibot.Claim(repo, 'P1540', is_qualifier=True)
         newClaim_men.setTarget(pywikibot.WbQuantity(int(split[2]), site=repo))
-        data['claims'][0]['qualifiers']['P1540'] = [newClaim_men.toJSON()]
+        newClaim.addQualifier(newClaim_men)
 
-        newClaim_women = pywikibot.Claim(repo, 'P1539', isQualifier=True)
+        newClaim_women = pywikibot.Claim(repo, 'P1539', is_qualifier=True)
         newClaim_women.setTarget(pywikibot.WbQuantity(int(split[3]), site=repo))
-        data['claims'][0]['qualifiers']['P1539'] = [newClaim_women.toJSON()]
+        newClaim.addQualifier(newClaim_women)
 
-        ref = pywikibot.Claim(repo, 'P248', isReference=True)
+        ref = pywikibot.Claim(repo, 'P248', is_reference=True)
         ref.setTarget(pywikibot.ItemPage(repo, ref_item))
-        data['claims'][0]['references'][0]['snaks']['P248'] = [ref.toJSON()]
 
         now = datetime.now()
-        access_date = pywikibot.Claim(repo, 'P813', isReference=True)
-        access_date.setTarget(pywikibot.WbTime(year=now.year, month=now.month, day=now.day, site=repo))
-        data['claims'][0]['references'][0]['snaks']['P813'] = [access_date.toJSON()]
+        access_date = pywikibot.Claim(repo, 'P813', is_reference=True)
+        access_date.setTarget(pywikibot.WbTime(year=now.year, month=now.month,
+                                               day=now.day, site=repo))
+        newClaim.addSources([ref, access_date])
 
+        data = {'claims':[newClaim.toJSON()]}
         for upToDateClaim in upToDateClaims:
             data['claims'].append(upToDateClaim.toJSON())
 
