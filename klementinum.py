3b7a37a4d72c3da2ae7971907c37016db44f5e27
Matěj Suchánek <matejsuchanek97@gmail.com>
2019-09-09 18:08:10 +0200

    add klementinum.py

---
+++ klementinum.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import requests
+
+import pywikibot
+import mwparserfromhell as parser
+
+from .lua_formatter import format_dictionary
+
+
+def get_single_year(year):
+    return year.rpartition(', ')[2]
+
+
+def main():
+    pywikibot.handle_args()
+    site = pywikibot.Site()
+    url_pattern = 'http://portal.chmi.cz/files/portal/docs/meteo/ok/extrklem{0}_cs.html'
+    text = '-- Zdroj dat:'
+    data = {}
+    for i in range(1, 13):
+        url = url_pattern.format('%02d' % i)
+        response = requests.get(url)
+        code = parser.parse(response.text)
+        month = {}
+        trs = (tr for tr in code.ifilter_tags() if tr.tag == 'tr')
+        next(trs)  # skip headline
+        for day, tr in enumerate(trs, start=1):
+            tags = tr.contents.filter_tags()
+            if len(tags) != 6:
+                break
+            avg, mx, mx_year, mn, mn_year = [tag.contents for tag in tags][1:]
+            mn_year, mx_year = map(get_single_year, (mn_year, mx_year))
+            month[day] = {
+                'avg': avg,
+                'max': mx,
+                'max_year': mx_year,
+                'min': mn,
+                'min_year': mn_year,
+            }
+        data[i] = month
+        text += '\n-- ' + url
+    page = pywikibot.Page(site, 'Modul:Klementinum/data')
+    text += '\n\nreturn ' + format_dictionary(
+        data, quotes_always=True, use_tabs=True)
+    page.put(text, summary='aktualizace dat pro šablonu Klementinum',
+             minor=False, botflag=False, apply_cosmetic_changes=False)
+
+
+if __name__ == '__main__':
+    main()
---
+++ lua_formatter.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+__all__ = (
+    'format_dictionary',
+    'format_list',
+    'QUOTES_SINGLE',
+    'QUOTES_DOUBLE',
+)
+
+QUOTES_SINGLE = 1
+QUOTES_DOUBLE = 2
+
+
+def _indent(level, **kwargs):
+    if kwargs.get('use_tabs') is True:
+        return (level + 1) * '\t'
+    else:
+        return ((level + 1) * 4) * ' '
+
+
+def _wrap_quotes(text, quote):
+    if quote in text:
+        text = text.replace(quote, '\\' + quote)
+    return '{0}{1}{0}'.format(quote, text)
+
+
+def _format_string(text, **kwargs):
+    opt = kwargs.get('force_quotes', 0)
+    if opt == QUOTES_SINGLE:
+        text = _wrap_quotes(text, "'")
+    elif opt == QUOTES_DOUBLE:
+        text = _wrap_quotes(text, '"')
+    else:
+        assert opt == 0
+        text = (_wrap_quotes(text, '"')
+                if "'" in text else _wrap_quotes(text, "'"))
+    return text
+
+
+def _format_key(key, **kwargs):
+    if key is None:
+        return '[nil]'
+    elif isinstance(key, (int, float)):
+        return '[%s]' % str(key).lower()  # lower for booleans (which are ints)
+    else:
+        assert isinstance(key, str)
+        if not key.isalnum() or kwargs.get('quotes_always') is True:
+            return '[%s]' % _format_string(key, **kwargs)
+        else:
+            return key
+
+
+def _format_value(value, level, **kwargs):
+    if isinstance(value, dict):
+        return _format_dictionary(value, level + 1, **kwargs)
+    elif isinstance(value, (list, tuple)):
+        return _format_list(value, level + 1, **kwargs)
+    elif isinstance(value, (int, float)):
+        return str(value).lower()  # lower for booleans (which are ints)
+    else:
+        return _format_string(value, **kwargs)
+
+
+def _format_pair(key, value, level, **kwargs):
+    return '%s = %s,' % (_format_key(key, **kwargs),
+                         _format_value(value, level, **kwargs))
+
+
+def _format_list(data, level, **kwargs):
+    init = '\n' + _indent(level, **kwargs)
+    string = ''
+    if kwargs.get('show_keys') is True:
+        for i, item in enumerate(data, start=1):
+            string += init + _format_pair(i, item, **kwargs)
+    else:
+        for item in data:
+            string += init + _format_value(item, level, **kwargs) + ','
+    return '{' + string + '\n' + _indent(level-1, **kwargs) + '}'
+
+
+def _format_dictionary(data, level, **kwargs):
+    init = '\n' + _indent(level, **kwargs)
+    string = ''
+    keys = data.keys()
+    if kwargs.get('sort_keys') is True:
+        keys = sorted(keys)
+    for key in keys:
+        string += init + _format_pair(key, data[key], level, **kwargs)
+    return '{' + string + '\n' + _indent(level-1, **kwargs) + '}'
+
+
+def format_list(data, level=0, **kwargs):
+    assert isinstance(data, (list, tuple))
+    return _format_list(data, level, **kwargs)
+
+
+def format_dictionary(data, level=0, **kwargs):
+    assert isinstance(data, dict)
+    return _format_dictionary(data, level, **kwargs)
