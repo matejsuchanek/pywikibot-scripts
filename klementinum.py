0cef2bb02525a6fa26c96317d0043bbf516fb77f
Matěj Suchánek <matejsuchanek97@gmail.com>
2020-03-11 18:48:32 +0100

    enforce dict key order in klementinum.py

--- klementinum.py
+++ klementinum.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from collections import OrderedDict
+
 import requests
 
 import pywikibot
@@ -18,12 +20,12 @@ def main():
     site = pywikibot.Site()
     url_pattern = 'http://portal.chmi.cz/files/portal/docs/meteo/ok/extrklem{0}_cs.html'
     text = '-- Zdroj dat:'
-    data = {}
+    data = OrderedDict()
     for i in range(1, 13):
         url = url_pattern.format('%02d' % i)
         response = requests.get(url)
         code = parser.parse(response.text)
-        month = {}
+        month = OrderedDict()
         trs = (tr for tr in code.ifilter_tags() if tr.tag == 'tr')
         next(trs)  # skip headline
         for day, tr in enumerate(trs, start=1):
@@ -32,13 +34,12 @@ def main():
                 break
             avg, mx, mx_year, mn, mn_year = [tag.contents for tag in tags][1:]
             mn_year, mx_year = map(get_single_year, (mn_year, mx_year))
-            month[day] = {
-                'avg': avg,
-                'max': mx,
-                'max_year': mx_year,
-                'min': mn,
-                'min_year': mn_year,
-            }
+            month[day] = OrderedDict()
+            month[day]['avg'] = avg
+            month[day]['max'] = mx
+            month[day]['max_year'] = mx_year
+            month[day]['min'] = mn
+            month[day]['min_year'] = mn_year
         data[i] = month
         text += '\n-- ' + url
     page = pywikibot.Page(site, 'Modul:Klementinum/data')
